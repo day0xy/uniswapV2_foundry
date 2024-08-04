200dd3343b14a159e182f3905373659d25b49b26
day0xy <jasonfan1337@gmail.com>
2024-08-04 10:32:16 +0800

    refactor

--- src/UniswapV2Migrator.sol
+++ src/UniswapV2Migrator.sol
@@ -1,6 +1,6 @@
-pragma solidity =0.6.6;
+pragma solidity >=0.6.6;
 
-import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
+import './libraries/TransferHelper.sol';
 
 import './interfaces/IUniswapV2Migrator.sol';
 import './interfaces/V1/IUniswapV1Factory.sol';
@@ -12,7 +12,7 @@ contract UniswapV2Migrator is IUniswapV2Migrator {
     IUniswapV1Factory immutable factoryV1;
     IUniswapV2Router01 immutable router;
 
-    constructor(address _factoryV1, address _router) public {
+    constructor(address _factoryV1, address _router)  {
         factoryV1 = IUniswapV1Factory(_factoryV1);
         router = IUniswapV2Router01(_router);
     }
