9f6e6928e9d19e7d471af17ef9c6810de4519adf
day0xy <jasonfan1337@gmail.com>
2024-08-04 10:20:51 +0800

    refactor

--- src/UniswapV2Factory.sol
+++ src/UniswapV2Factory.sol
@@ -1,27 +1,24 @@
-pragma solidity =0.5.16;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.5.16;
 
 import "./interfaces/IUniswapV2Factory.sol";
 import "./UniswapV2Pair.sol";
 
 contract UniswapV2Factory is IUniswapV2Factory {
+    //The feeTo address accumulates the liquidity tokens for the protocol fee
+    //feeToSetter is the address allowed to change feeTo to a different address.
     address public feeTo;
     address public feeToSetter;
 
     mapping(address => mapping(address => address)) public getPair;
+    //an array that includes all the addresses of pair exchanges created by this factory.
     address[] public allPairs;
-
-    event PairCreated(
-        address indexed token0,
-        address indexed token1,
-        address pair,
-        uint
-    );
 
     constructor(address _feeToSetter) public {
         feeToSetter = _feeToSetter;
     }
 
-    function allPairsLength() external view returns (uint) {
+    function allPairsLength() external view returns (uint256) {
         return allPairs.length;
     }
 
@@ -30,6 +27,7 @@ contract UniswapV2Factory is IUniswapV2Factory {
         address tokenB
     ) external returns (address pair) {
         require(tokenA != tokenB, "UniswapV2: IDENTICAL_ADDRESSES");
+        // for deterministic address
         (address token0, address token1) = tokenA < tokenB
             ? (tokenA, tokenB)
             : (tokenB, tokenA);
